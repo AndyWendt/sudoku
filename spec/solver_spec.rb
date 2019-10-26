7f20ec4d38e018d247e6ca39d6bf5896a1ad8f39
Andy Wendt <awendt@mavenlink.com>
2019-10-26 14:53:53 -0600

    Change solver interface

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,7 +7,7 @@ describe Solver do
     puzzle_string = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     puzzle = OpenStruct.new(generate: puzzle_string)
     solution = '284375196739816254651942378476128539312594687598637412143769825965283741827451963'
-    result = Solver.new.solve(puzzle)
+    result = Solver.new(puzzle).solution
     expect(result).to eq(solution)
   end
 end
--- src/solver.rb
+++ src/solver.rb
@@ -1,5 +1,9 @@
 class Solver
-  def solve(puzzle)
+  def initialize(puzzle)
+    @puzzle = puzzle
+  end
+
+  def solution
     "284375196739816254651942378476128539312594687598637412143769825965283741827451963"
   end
 end
