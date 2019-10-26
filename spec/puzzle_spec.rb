b4b8214896ed7f5be71cc61adbdf8fe1f75e4987
Andy Wendt <awendt@mavenlink.com>
2019-10-26 14:30:36 -0600

    Add puzzle expectation

--- spec/puzzle_spec.rb
+++ spec/puzzle_spec.rb
@@ -6,5 +6,6 @@ describe Puzzle do
     result = Puzzle.new.generate
     expected = "2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"
     expect(result.length).to eq(81)
+    expect(result).to eq(expected)
   end
 end
