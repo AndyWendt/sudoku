12a2d5f063a702370f1bf885ff34717784dc3ec9
Andy Wendt <awendt@mavenlink.com>
2019-10-26 14:20:28 -0600

    Puzzle generation returns a string

--- spec/puzzle_spec.rb
+++ spec/puzzle_spec.rb
@@ -4,19 +4,7 @@ require './src/puzzle'
 describe Puzzle do
   it 'creates a puzzle' do
     result = Puzzle.new.generate
-    expected = [
-        [0,0,7,4,3,5,6,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-    ]
-
-    result.each { |row| expect(row.length).to eq(9) }
-    expect(result.length).to eq(9)
+    expected = "2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"
+    expect(result.length).to eq(81)
   end
 end
