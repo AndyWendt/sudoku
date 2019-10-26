07a3446a87ea32d79a5941052e5c4633db4d6af2
Andy Wendt <awendt@mavenlink.com>
2019-10-26 13:53:22 -0600

    Add puzzle

--- spec/puzzle_spec.rb
+++ spec/puzzle_spec.rb
@@ -4,21 +4,19 @@ require './src/puzzle'
 describe Puzzle do
   it 'creates a puzzle' do
     result = Puzzle.new.generate
-    # array of arrays
-    # array of hashes
-    # array of objects that are cells
-    # row, column, grid
     expected = [
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
+        [0,0,7,4,3,5,6,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
     ]
-    expect(result).to eq(expected)
+
+    result.each { |row| expect(row.length).to eq(9) }
+    expect(result.length).to eq(9)
   end
 end
