53a11d6e969ae81a051a9a74a3c8b627a34e3da6
Andy Wendt <awendt@mavenlink.com>
2019-10-26 14:58:08 -0600

    Add solvable method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,9 +6,13 @@ describe Solver do
   let(:puzzle) {OpenStruct.new(generate: puzzle_string)}
   let(:puzzle_string) { '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3' }
   let(:solution) { '284375196739816254651942378476128539312594687598637412143769825965283741827451963' }
+  let(:solvable) {true}
 
   it 'solves a puzzle' do
-    result = Solver.new(puzzle).solution
-    expect(result).to eq(solution)
+    expect(Solver.new(puzzle).solution).to eq(solution)
+  end
+
+  it 'determines if a puzzle is solvable' do
+    expect(Solver.new(puzzle).solvable).to eq(solvable)
   end
 end
