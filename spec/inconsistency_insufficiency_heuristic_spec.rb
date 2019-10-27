1bd162168b632fd419608520745a6b365f19bcfd
Andy Wendt <awendt@mavenlink.com>
2019-10-27 16:58:13 -0600

    Add inconsistency insufficiency heuristic

---
+++ spec/inconsistency_insufficiency_heuristic_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'ostruct'
+require './src/inconsistency_insufficiency_heuristic'
+require './src/puzzle'
+require './src/puzzle_cell_intelligence'
+
+describe InconsistencyInsufficiencyHeuristic do
+  let(:puzzle_string) { '...26.7.168..7..9.19...45..82.1...4...46.29...5...3.28..93...74.4..5..367.3.18...' }
+  let(:puzzle) { Puzzle.new(puzzle_string) }
+  let(:cell) { 0 }
+  let(:potential_cell_values) { [4] }
+  let(:puzzle_cell_intelligence) do
+    pci = double("PuzzleCellIntelligence")
+    allow(pci).to receive(:potential_cell_values).and_return(potential_cell_values)
+    pci
+  end
+
+  subject { InconsistencyInsufficiencyHeuristic.new(puzzle, puzzle_cell_intelligence) }
+
+  it 'executes' do
+    subject.execute(cell)
+    expect(puzzle.get(0).to_i).to eq(4)
+  end
+end
---
+++ src/inconsistency_insufficiency_heuristic.rb
@@ -0,0 +1,13 @@
+class InconsistencyInsufficiencyHeuristic
+  def initialize(puzzle, puzzle_cell_intelligence)
+    @puzzle = puzzle
+    @puzzle_cell_intelligence = puzzle_cell_intelligence
+  end
+
+  def execute(cell)
+    potential_values = @puzzle_cell_intelligence.potential_cell_values(cell)
+    if potential_values.length == 1
+      @puzzle.set(cell, potential_values.first.to_s)
+    end
+  end
+end
