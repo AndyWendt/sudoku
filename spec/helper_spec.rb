83d5663d2383efc0f499950862fe4bd80e6477b7
Andy Wendt <awendt@mavenlink.com>
2019-10-26 20:46:58 -0600

    Find next lowest works with default values

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -5,6 +5,13 @@ describe Helper do
   let(:values) {[0,3,6,27,30,33,54,57,60]}
 
   it 'finds the next lowest number in an array' do
+    expect(Helper.find_next_lowest(0, values)).to eq(0)
+    expect(Helper.find_next_lowest(3, values)).to eq(3)
+    expect(Helper.find_next_lowest(6, values)).to eq(6)
+    expect(Helper.find_next_lowest(27, values)).to eq(27)
     expect(Helper.find_next_lowest(30, values)).to eq(30)
+    expect(Helper.find_next_lowest(54, values)).to eq(54)
+    expect(Helper.find_next_lowest(57, values)).to eq(57)
+    expect(Helper.find_next_lowest(60, values)).to eq(60)
   end
 end
--- src/helper.rb
+++ src/helper.rb
@@ -1,11 +1,25 @@
 class Helper
   def self.find_next_lowest(value, array)
-    length = array.length
-    middle_index = length / 2
+    upper_index = array.length
+    middle_index = upper_index / 2
+    lower_index = 0
     found = false
     until found
       middle_value = array[middle_index]
-      return middle_value if middle_value == value
+      return middle_value if middle_value == value || (upper_index - lower_index) < 1
+
+      # lower
+      if value < middle_value
+        upper_index = middle_index - 1
+      end
+
+      # higher
+      if value > middle_value
+        lower_index = middle_index + 1
+      end
+
+      length = upper_index - lower_index
+      middle_index = (length / 2) + lower_index
     end
   end
 end
