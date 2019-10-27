8ab5af18afca175432d48fcb71c212e10e987f48
Andy Wendt <awendt@mavenlink.com>
2019-10-26 20:22:03 -0600

    Find middle value by default

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -5,6 +5,6 @@ describe Helper do
   let(:values) {[0,3,6,27,30,33,54,57,60]}
 
   it 'finds the next lowest number in an array' do
-    expect(Helper.find_next_lowest(80, values)).to eq(60)
+    expect(Helper.find_next_lowest(30, values)).to eq(30)
   end
 end
--- src/helper.rb
+++ src/helper.rb
@@ -1,5 +1,11 @@
 class Helper
   def self.find_next_lowest(value, array)
-    60
+    length = array.length
+    middle_index = length / 2
+    found = false
+    until found
+      middle_value = array[middle_index]
+      return middle_value if middle_value == value
+    end
   end
 end
