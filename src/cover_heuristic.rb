db667c7fcbda8bd90ae3fc5656f1953bb1b522ed
Andy Wendt <awendt@mavenlink.com>
2019-10-27 21:27:48 -0600

    Set puzzle in cover heuristic

--- src/cover_heuristic.rb
+++ src/cover_heuristic.rb
@@ -4,19 +4,33 @@ class CoverHeuristic
   end
 
   def execute(puzzle, cell)
+    @puzzle = puzzle
     hash = ''
-    while puzzle.changed(hash)
-      hash = puzzle.hash
-      handle_cover_values(puzzle, cell)
-      handle_single_potential_cell_values(puzzle, cell)
+    while @puzzle.changed(hash)
+      hash = @puzzle.hash
+      handle_row_cover_values(cell)
+      handle_single_potential_cell_values(cell)
     end
   end
 
   private
 
-  def handle_cover_values(puzzle, cell)
-    row_cell_values = @puzzle_cell_intelligence.potential_row_cell_values(puzzle, cell)
-    uniques = row_cell_values.each_with_object({}) do |(index, values), uniques|
+  def handle_row_cover_values(cell)
+    row_cell_values = @puzzle_cell_intelligence.potential_row_cell_values(@puzzle, cell)
+    handle_cell_value_covers(row_cell_values)
+  end
+
+  def handle_single_potential_cell_values(cell)
+    prcv = @puzzle_cell_intelligence.potential_row_cell_values(@puzzle, cell)
+    prcv.each_with_index do |(cell, potential_values), _|
+          next unless potential_values.length == 1
+
+          @puzzle.set(cell, potential_values.first.to_s)
+        end
+  end
+
+  def handle_cell_value_covers(cell_values)
+    uniques = cell_values.each_with_object({}) do |(index, values), uniques|
       values.each do |value|
         (uniques[value] ||= []) << index
       end
@@ -25,16 +39,7 @@ class CoverHeuristic
     uniques.each.with_index do |(value, cells), _|
       next unless cells.length == 1
 
-      puzzle.set(cells.first, value.to_s)
+      @puzzle.set(cells.first, value.to_s)
     end
-  end
-
-  def handle_single_potential_cell_values(puzzle, cell)
-    prcv = @puzzle_cell_intelligence.potential_row_cell_values(puzzle, cell)
-    prcv.each_with_index do |(cell, potential_values), _|
-          next unless potential_values.length == 1
-
-          puzzle.set(cell, potential_values.first.to_s)
-        end
   end
 end
