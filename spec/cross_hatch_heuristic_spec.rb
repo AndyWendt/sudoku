5a248ff4b4ce2c1da6f0581c322f418da8e5b102
Andy Wendt <awendt@mavenlink.com>
2019-11-03 00:48:37 -0600

    Cross hatch working

--- spec/cross_hatch_heuristic_spec.rb
+++ spec/cross_hatch_heuristic_spec.rb
@@ -7,16 +7,17 @@ describe CrossHatchHeuristic do
   # https://dingo.sbs.arizona.edu/~sandiway/sudoku/examples.html
   let(:puzzle_string) { '...26.7.168..7..9.19...45..82.1...4...46.29...5...3.28..93...74.4..5..367.3.18...' }
   let(:puzzle) { Puzzle.new(puzzle_string) }
-  let(:potential_cell_values) { [4] }
+  let(:cell_candidates) { [4] }
   let(:puzzle_cell_intelligence) { PuzzleCellIntelligence.instance }
 
   subject { CrossHatchHeuristic.new(puzzle_cell_intelligence) }
 
   it 'finds hidden singles' do
-    skip
     subject.execute(puzzle, 80)
     subject.execute(puzzle, 0)
+    subject.execute(puzzle, 54)
     expect(puzzle.get(80)).to eq(9)
+    expect(puzzle.get(54)).to eq(5)
     expect(puzzle.get(0)).to eq(4)
   end
 
@@ -28,24 +29,25 @@ describe CrossHatchHeuristic do
   end
 
   describe "candidates" do
+    let(:cell) { 43 }
     it 'sets candidates for the cell if there is not a single value' do
-      subject.execute(puzzle, 54)
-      expect(puzzle.get(54)).to eq(nil)
-      expect(puzzle.candidates(54)).to eq([2,5])
+      subject.execute(puzzle, cell)
+      expect(puzzle.get(cell)).to eq(nil)
+      expect(puzzle.candidates(cell)).to eq([1,5])
     end
 
     it 'does not set the candidates if they are already set' do
-      puzzle.candidates(54, [2])
-      subject.execute(puzzle, 54)
-      expect(puzzle.get(54)).to eq(nil)
-      expect(puzzle.candidates(54)).to eq([2])
+      puzzle.candidates(cell, [1])
+      subject.execute(puzzle, cell)
+      expect(puzzle.get(cell)).to eq(nil)
+      expect(puzzle.candidates(cell)).to eq([1])
     end
 
     it 'removes candidates for the cell if they do not exist in the new candidates' do
-      puzzle.candidates(54, [2, 5, 3])
-      subject.execute(puzzle, 54)
-      expect(puzzle.get(54)).to eq(nil)
-      expect(puzzle.candidates(54)).to eq([2, 5])
+      puzzle.candidates(cell, [2, 5, 3])
+      subject.execute(puzzle, cell)
+      expect(puzzle.get(cell)).to eq(nil)
+      expect(puzzle.candidates(cell)).to eq([5])
     end
   end
 end
--- src/cross_hatch_heuristic.rb
+++ src/cross_hatch_heuristic.rb
@@ -5,14 +5,41 @@ class CrossHatchHeuristic
 
   def execute(puzzle, cell)
     find_naked_singles(cell, puzzle)
+    find_hidden_singles(cell, puzzle)
   end
 
   private
 
   def find_naked_singles(cell, puzzle)
-    candidates = @puzzle_cell_intelligence.potential_cell_values(puzzle, cell)
+    candidates = @puzzle_cell_intelligence.cell_candidates(puzzle, cell)
     puzzle.set(cell, candidates[0]) if candidates.length == 1
     merge_candidates(puzzle.candidates(cell), candidates, puzzle, cell)
+  end
+
+  def find_hidden_singles(cell, puzzle)
+    counts = {
+      1 => [],
+      2 => [],
+      3 => [],
+      4 => [],
+      5 => [],
+      6 => [],
+      7 => [],
+      8 => [],
+      9 => [],
+    }
+
+    @puzzle_cell_intelligence
+      .grid_candidates(puzzle, cell)
+      .reduce(counts) do |counts, (cell_location, candidates)|
+        candidates.each { |candidate_value| counts[candidate_value].push(cell_location) }
+        counts
+      end
+
+    counts.each do |number, cells|
+      next unless cells.length == 1
+      puzzle.set(cells[0], number)
+    end
   end
 
   def merge_candidates(old, new, puzzle, cell)
